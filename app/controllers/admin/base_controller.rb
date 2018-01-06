131592ec7086224d6ce7bf29d8c4f24dc532f02b
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2018-01-06 18:01:28 +0900

    Require login on admin page

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,11 @@
 class Admin::BaseController < ApplicationController
   layout "admin"
+
+  before_action :require_login
+
+  def require_login
+    if session[:user_id].nil? || User.find(session[:user_id]).nil?
+      redirect_to root_path, alert: "You must be logged in!"
+    end
+  end
 end
