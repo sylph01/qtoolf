3966bcca9bcb41de5355b3ecbf389189ba2483e9
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2018-01-06 18:27:35 +0900

    User.find raises exception when not found

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,7 +4,7 @@ class Admin::BaseController < ApplicationController
   before_action :require_login
 
   def require_login
-    if session[:user_id].nil? || User.find(session[:user_id]).nil?
+    if session[:user_id].nil? || User.find_by_id(session[:user_id]).nil?
       redirect_to root_path, alert: "You must be logged in!"
     end
   end
